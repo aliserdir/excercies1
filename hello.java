59fe5b81821064b6f38b597c2aaad004eefb2eca
A. Serdar Yilmaz <aliserdir@gmail.com>
2024-07-04 10:36:59 +0200

    Zweite Version

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Daniel");
+        System.out.println("Raimund");
+        System.out.println("Ali");
+        System.out.println("Room2");
+        
     }
 }
